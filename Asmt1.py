ff1364966281ceadbed5314b623c167d321022a7
deepanshi-billore <59094415+deepanshi-billore@users.noreply.github.com>
2022-06-08 18:31:25 +0530

    Add files via upload

---
+++ Asmt1.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+# 
+# 
+# 
+# *           = expression
+# 
+# 
+# hello       = values
+# 
+# 
+# -87.8       = values
+# 
+# 
+# /           = expression
+# 
+# 
+# +           = expression
+# 
+# 
+# 6           = values
+# 
+# 
+# -           = expression
+
+# In[ ]:
+
+
+
+
+
+# 2. What is the difference between string and variable?
+# 
+# 
+# Variable is a block which is used to store any kind of data.
+# String is a series of character that can be stored in a variable.
+
+# In[ ]:
+
+
+
+
+
+# 3. Data Types
+# 
+# String : Series of character can be defined as a string. String is immutable.
+#     
+#     
+# List : List is an oredered collection of data and are mutable
+#     
+# 
+# Tuple : Tuple is similar to list but they are immutable
+#     
+#     
+
+# In[ ]:
+
+
+
+
+
+# 4. expression
+# 
+# 
+# An expression is made up of variables,operations and result.
+# 
+# 
+# Expression is used to represent values.
+
+# In[ ]:
+
+
+
+
+
+# 5. Difference between expression and statement
+# 
+# 
+# Statements represent a command.
+# 
+# 
+# Expression is a combination of variables, operations and values
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+#q6
+
+bacon=22
+bacon+1
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+#q7
+
+"spam"+"spamspam"
+
+
+# In[4]:
+
+
+"spam"*3
+
+
+# In[ ]:
+
+
+
+
+
+# 8 Why is eggs a valid variable name while 100 is invalid?
+# 
+# 
+# Because a variable can only start with either an alphabet or an underscore(-).
+# 
+# No special characters or digits are allowed in a variable name's beginning.
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+# 
+# 
+# int()
+# 
+# float()
+# 
+# str()
+
+# In[ ]:
+
+
+
+
+
+# 10. Why does this expression cause an error? How can you fix it?
+# 
+# 
+
+# 'I have eaten'; + 99 + 'burritos'
+
+# 
+# 
+# This expression will give an error because we cann't concatenate a string and an integer.
+# 
+# 
+
+# In[11]:
+
+
+'I have eaten'+ " 99 " +"buritos"
+
+
+# In[ ]:
+
+
+
+
