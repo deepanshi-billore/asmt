7a0b620fd2085de86f0e7470b699dcdb56ded858
deepanshi-billore <59094415+deepanshi-billore@users.noreply.github.com>
2022-06-16 23:48:42 +0530

    Add files via upload

---
+++ ProgAsmt1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Write a Python program to print "Hello Python"?
+
+# In[1]:
+
+
+print("Hello Python")
+
+
+# 2. Write a Python program to do arithmetical operations addition and division.?
+
+# In[2]:
+
+
+a=11
+b=18
+c=a+b
+d=a/b
+print(c)
+print(d)
+
+
+# 3. Write a Python program to find the area of a triangle?
+
+# In[3]:
+
+
+b=11
+h=18
+a=(b*h)/2
+print(a)
+
+
+# 4. Write a Python program to swap two variables?
+
+# In[4]:
+
+
+a=11
+b=18
+print(a)
+print(b)
+s=a
+a=b
+b=s
+print("After Swapping")
+print(a)
+print(b)
+
+
+# 5. Write a Python program to generate a random number?
+
+# In[13]:
+
+
+import random
+
+
+print(random.randint(11,18))
+
+
+# In[ ]:
+
+
+
+
